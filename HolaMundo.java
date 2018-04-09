3f0c2ab670e79a1a61c9ca702061d0a873e74622
Capacho <mirationonsei@gmail.com>
2018-04-09 12:18:08 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,9 @@
 
 public class HolaMundo{
 	public static void main(String[] args){
+		String naranja="\033[33m";
+		String azul = "\033[34m";
+
 		System.out.println("¡Hola mundo!");
 		System.out.println("Linea de proba para version");
 	}
