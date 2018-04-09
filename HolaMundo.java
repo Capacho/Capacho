70577e152b6b67d6068279c27902cd3107ed7ea4
Capacho <mirationonsei@gmail.com>
2018-04-09 12:19:30 +0200

    Saludo en colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo{
 		String naranja="\033[33m";
 		String azul = "\033[34m";
 
-		System.out.println("¡Hola mundo!");
-		System.out.println("Linea de proba para version");
+		System.out.println(naranja+"¡Hola mundo!");
+		System.out.println(azul+"Linea de proba para version");
 	}
 }
